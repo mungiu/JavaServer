8a16f6db2365993afea49d1b2ca5374009bdc677
PeipeiSu <266128@via.dk>
2019-07-06 17:29:49 +0200

    controller method fixed

--- src/main/java/Controller/ILocationController.java
+++ src/main/java/Controller/ILocationController.java
@@ -6,7 +6,7 @@ import Model.LocationList;
 import java.sql.SQLException;
 
 public interface ILocationController {
-    void assignLocationToCompany(String locationID, String companyID) throws SQLException;
+    void assignLocationToCompany(String locationID, String companyID, String rentalStart, String rentalEnd) throws SQLException;
     void removeLocationFromCurrentCompany(String locationID, String companyID) throws SQLException;
     Location getLocationByID(String locationID) throws SQLException;
     LocationList getAvailableLocations() throws SQLException;
