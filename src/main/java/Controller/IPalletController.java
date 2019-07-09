cd6debb07378ab6330e92d5194612fba03d914a9
PeipeiSu <266128@via.dk>
2019-07-09 19:41:10 +0200

    updated

--- src/main/java/Controller/IPalletController.java
+++ src/main/java/Controller/IPalletController.java
@@ -1,5 +1,9 @@
 package Controller;
 
+import java.sql.SQLException;
+
+import Model.Company;
+import Model.Location;
 import Model.Pallet;
 import Model.PalletList;
 
@@ -8,7 +12,7 @@ import java.sql.SQLException;
 public interface IPalletController {
 
     Pallet getPalletByID(String palletID, String companyID, String locationID ) throws SQLException;
-    void removePallet(String palletID, String companyID) throws SQLException;
+    void removePallet(String palletID, String companyID)throws SQLException;
     void StorePallet(Pallet pallet, String companyID, String locationID)throws SQLException;
     PalletList getPalletList() throws SQLException;
 }
